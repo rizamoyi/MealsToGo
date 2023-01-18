9a4d2f5224d37bd417226d50d35934cbda843f7d
rizamoyi <samwelmoyi@gmail.com>
2023-01-18 22:55:56 +0300

    Restaurant info component

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,4 +1,34 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
 
-export const RestaurantInfo = () => <Text>Restaurant Info</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+    <Card style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      {/* <Text>{name}</Text> */}
+      <Card.Title title={name} />
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "blue",
+  },
+  cover: {
+    padding: 20,
+    backgroundColor: "white",
+  },
+});
