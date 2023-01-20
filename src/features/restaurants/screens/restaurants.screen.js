35454116c62ffb8a207ba9f45303478649f53206
rizamoyi <samwelmoyi@gmail.com>
2023-01-20 18:26:18 +0300

    Setup and Customize theme

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -7,18 +7,16 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  background-color: #fff;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  background-color: green;
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => (
---
+++ src/infrastructure/theme/index.js
@@ -0,0 +1,14 @@
+import { fonts, fontWeights, fontSizes } from "./fonts";
+import { space, lineHeights } from "./spacing";
+import { colors } from "./colors";
+import { sizes } from "./sizes";
+
+export const theme = {
+  lineHeights,
+  fontWeights,
+  fontSizes,
+  colors,
+  fonts,
+  space,
+  sizes,
+};
