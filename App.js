f17b93928445d18cca5b0bf974a09359f13544d6
rizamoyi <samwelmoyi@gmail.com>
2023-01-24 14:35:18 +0300

    Added react navigation and it's depenendancies and setup

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ThemeProvider } from "styled-components/native";
+import { View, Text } from "react-native";
 
 import {
   useFonts as useOswald,
@@ -10,6 +13,16 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -25,11 +38,14 @@ export default function App() {
   }
 
   return (
-    <>
+    <NavigationContainer>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Restaurants" component={RestaurantsScreen} />
+        </Stack.Navigator>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
-    </>
+    </NavigationContainer>
   );
 }
