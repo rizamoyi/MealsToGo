8dda440bdd5e608229ae7db254744491dbd824f7
rizamoyi <samwelmoyi@gmail.com>
2023-01-26 22:01:09 +0300

    Restaurant Detail navigation

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Text } from "react-native";
 
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
 
 const RestaurantStack = createStackNavigator();
+
+const RestaurantDetailScreen = () => {
+  return <Text>Restaurant Detail</Text>;
+};
 
 export const RestaurantsNavigator = () => {
   return (
@@ -14,7 +19,14 @@ export const RestaurantsNavigator = () => {
       }}
       name="Restaurants"
     >
-      <RestaurantStack.Screen name="Helloworld" component={RestaurantsScreen} />
+      <RestaurantStack.Screen
+        name="All Restaurants"
+        component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={RestaurantDetailScreen}
+      />
     </RestaurantStack.Navigator>
   );
 };
