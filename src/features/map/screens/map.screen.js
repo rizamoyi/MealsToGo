b63228fd4690138147b80d6b7d684aba6477c5ea
rizamoyi <samwelmoyi@gmail.com>
2023-01-28 12:34:36 +0300

    Set Map region

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
+
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants.context";
 
 import { Search } from "../components/search.component";
 
@@ -10,10 +13,36 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
+  console.log(viewport.northeast);
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    const latDelta = northeastLat - southwestLat;
+    setLatDelta(latDelta);
+  }, [location, viewport]);
+
   return (
     <>
       <Search />
-      <Map />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
     </>
   );
 };
