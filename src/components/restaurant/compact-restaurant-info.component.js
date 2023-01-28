f82c84b3ceeb16b29111e12a96633715ca467047
rizamoyi <samwelmoyi@gmail.com>
2023-01-28 13:49:53 +0300

    Add Custom map callout

---
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components/native";
+import { WebView } from "react-native-webview";
+import { Platform } from "react-native";
+
+import { Text } from "../typography/text.component";
+
+const CompactImage = styled.Image`
+  border-radius: ${(props) => props.theme.space[1]};
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebView = styled(WebView)`
+  border-radius: ${(props) => props.theme.space[1]};
+  width: 120px;
+  height: 100px;
+`;
+
+const Item = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+  max-width: 120px;
+  align-items: center;
+`;
+
+const isAndroid = Platform.OS === "android";
+export const CompactRestaurantInfo = ({ restaurant }) => {
+  const Image = isAndroid ? CompactWebView : CompactImage;
+  return (
+    <Item>
+      <Image source={{ uri: restaurant.photos[0] }} />
+      <Text>{restaurant.name}</Text>
+    </Item>
+  );
+};
