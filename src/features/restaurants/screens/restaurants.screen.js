06e4198537366c36d66b992f39d02b9c86664b0e
rizamoyi <samwelmoyi@gmail.com>
2023-02-06 23:53:10 +0300

    Animations and polish

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -2,6 +2,8 @@ import React, { useState, useContext } from "react";
 import { TouchableOpacity } from "react-native";
 import { MD2Colors } from "react-native-paper";
 
+import { FadeInView } from "../../../components/animations/fade.animation";
+
 import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
@@ -49,7 +51,9 @@ export const RestaurantsScreen = ({ navigation }) => {
             }
           >
             <Spacer position="bottom" size="large">
-              <RestaurantInfoCard restaurant={item} />
+              <FadeInView>
+                <RestaurantInfoCard restaurant={item} />
+              </FadeInView>
             </Spacer>
           </TouchableOpacity>
         )}
