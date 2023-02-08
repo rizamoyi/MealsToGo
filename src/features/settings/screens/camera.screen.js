cd4fcd6305cf4fb757c74a5f148b680311102be7
rizamoyi <samwelmoyi@gmail.com>
2023-02-08 13:13:23 +0300

    Take profile picture functionality

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,21 +1,63 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import styled from "styled-components/native";
 import { Camera, CameraType } from "expo-camera";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Button } from "react-native-paper";
+import { Text, View, TouchableOpacity } from "react-native";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
+  flex: 1;
+`;
+
+const InnerSnap = styled.View`
+  width: 100%;
+  height: 100%;
+  z-index: 999;
 `;
 
 export const CameraScreen = () => {
-  const [type, setType] = useState(CameraType.front);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef();
 
-  const toggleCameraType = () => {
-    setType((current) =>
-      current === CameraType.back ? CameraType.front : CameraType.back
-    );
+  const onSnap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+    }
   };
-  return <ProfileCamera type={type}></ProfileCamera>;
+
+  if (!permission) {
+    return (
+      <View>
+        <Text>No access to camera</Text>
+        <Button title="Grant camera permission" onPress={requestPermission}>
+          Grant Camera Permission
+        </Button>
+      </View>
+    );
+  }
+
+  if (!permission.granted) {
+    return (
+      <View>
+        <Text>No access to camera</Text>
+        <Button title="Grant camera permission" onPress={requestPermission}>
+          Grant Camera Permission
+        </Button>
+      </View>
+    );
+  }
+
+  return (
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={CameraType.front}
+      ratio={"16:9"}
+    >
+      <TouchableOpacity onPress={onSnap}>
+        <InnerSnap />
+      </TouchableOpacity>
+    </ProfileCamera>
+  );
 };
