ac68d1d113682aa35918ea269848283ebf550e25
rizamoyi <samwelmoyi@gmail.com>
2023-02-15 22:56:17 +0300

    Integrate Geocoding API

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,8 +1,28 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
-  const locationMock = locationsMock[city.toLowerCase()];
-  response.json(locationMock);
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    response.json(locationMock);
+  }
+
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: "",
+      },
+      timeout: 1000,
+    })
+    .then((r) => {
+      return response.json(r.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
+
+  // return response.status(400).send("You must set mock to true");
 };
